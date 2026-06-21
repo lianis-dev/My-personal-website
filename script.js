9f820a1519ce096c4c9f5c9d6a3812f1c9e0aacf
Lianis Silva Rodriguez <lianissr.collab@gmail.com>
2026-06-21 19:20:10 +0200

    Added effects and graphics
    I added a photo, a clickable folder image, and a menu icon. I improved the contact section and added a smooth scroll when clicking the "contact me" button with JavaScript. Lastly, I improved the menu and added a hover effect.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById('menuButton');
 const sidebar = document.getElementById('sidebar');
-const overlay = document.getElementById('overlay')
+const overlay = document.getElementById('overlay');
+const contactBtn = document.getElementById('contact-me-btn');
+const contactSection = document.getElementById('contact-section');
 
 function toggleMenu(){
     sidebar.classList.toggle('active');
@@ -9,3 +11,7 @@ function toggleMenu(){
 
 menuBtn.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
+
+contactBtn.addEventListener('click', function() {
+    contactSection.scrollIntoView({behavior: 'smooth'});
+});
